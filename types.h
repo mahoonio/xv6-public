af6bf4cab8eef0739ea3d5793ed32451fe354c82
mahan <m.mahmodi1380@gmail.com>
2022-11-28 23:34:30 +0330

    some basic setup for new Syscall.

--- types.h
+++ types.h
@@ -2,3 +2,10 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+typedef struct proc_info
+{
+    int pid;
+    int memsize; // in bytes
+}proc_info;
+
+
